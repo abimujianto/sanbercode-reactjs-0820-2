41ffda95858b30da2daf1234692f6e64df5f3dc5
abimujianto <abimujianto1997@gmail.com>
2020-09-04 19:37:53 +0800

    tugas 10

--- src/App.js
+++ src/App.js
@@ -1,37 +1,13 @@
 import React from "react";
-import "./App.css";
+import PembelianBuah from "../src/Tugas-9/Tugas-9";
+import Tugas10 from "../src/Tugas-10/Tugas-10";
 
 function App() {
   return (
-    <div className="container">
-      <div className="form">
-        <div className="content">
-          <h1>Form Pembelian Buah</h1>
-          <label className="name">Nama Pelanggan</label>
-          <input className="inputText" type="text" />
-          <br></br>
-          <br></br>
-          <div className="pilihan">
-            <input type="checkbox" />
-            <label>Semangka</label>
-            <br></br>
-            <input type="checkbox" />
-            <label>Jeruk</label>
-            <br></br>
-            <input type="checkbox" />
-            <label>Nanas</label>
-            <br></br>
-            <input type="checkbox" />
-            <label>Salak</label>
-            <br></br>
-          </div>
-          <label className="bold">Daftar Item</label>
-          <input type="checkbox" className="inAnggur" />
-          <label>Anggur</label>
-          <br></br>
-          <button className="btn">Kirim</button>
-        </div>
-      </div>
+    <div>
+      <PembelianBuah />
+
+      <Tugas10 />
     </div>
   );
 }
